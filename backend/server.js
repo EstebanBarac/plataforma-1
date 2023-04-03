d7f21b23310f76e1ae17f78c29f634f8207fba2f
Esteban Barac <EstebanBarac8@gmail.com>
2023-04-03 11:46:11 -0300

    sv

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
-const app = require("./app");
-const connectDatabase = require("./db/Database");
+const app = require('./app');
+const connectDatabase = require('./db/Database');
 
 // Handling uncaught Exception
-process.on("uncaughtException", (err) => {
+process.on('uncaughtException', (err) => {
   console.log(`Error: ${err.message}`);
   console.log(`shutting down the server for handling uncaught exception`);
 });
 
 // config
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({
-    path: "backend/config/.env",
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+  require('dotenv').config({
+    path: 'backend/config/.env',
   });
 }
 
@@ -19,13 +19,11 @@ connectDatabase();
 
 // create server
 const server = app.listen(process.env.PORT, () => {
-  console.log(
-    `Server is running on http://localhost:${process.env.PORT}`
-  );
+  console.log(`Server is running on https://cerise-badger-suit.cyclic.app/`);
 });
 
 // unhandled promise rejection
-process.on("unhandledRejection", (err) => {
+process.on('unhandledRejection', (err) => {
   console.log(`Shutting down the server for ${err.message}`);
   console.log(`shutting down the server for unhandle promise rejection`);
 
